8a94b01f854685d26131734a967f72fb2c584fed
David <davidnolasco544@gmail.com4>
2021-07-26 17:50:23 -0600

    header_member y verify_member

---
+++ member/header_member.php
@@ -0,0 +1,36 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700">
+		<link rel="stylesheet" type="text/css" href="css/header_member_style.css?v=<?PHP echo time();?>" />
+	</head>
+	<body>
+		<header>
+			<div id="cd-logo">
+				<a href="../">
+					<img src="img/RickLogo.svg" alt="Logo" />
+					<p>Librería Rick </p>
+					
+				</a>
+			</div>
+			
+			<div class="desplegar">
+				<button class="desplegarbtn">
+					<p id="libreria-nombre"><?php echo $_SESSION['username'] ?></p>
+				</button>
+				<div class="desplegar-contenido">
+					<a>
+						<?php
+							$query = $con->prepare("SELECT balance FROM member WHERE username = ?;");
+							$query->bind_param("s", $_SESSION['username']);
+							$query->execute();
+							$balance = (int)$query->get_result()->fetch_array()[0];
+							echo "Mi crédito: L.".$balance;
+						?>
+					</a>
+					<a href="my_books.php">Mis Libros</a>
+					<a href="../logout.php">Cerrar Sesión</a>
+				</div>
+			</div>
+		</header>
+	</body>
+</html>
