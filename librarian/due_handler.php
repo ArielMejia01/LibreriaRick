21440c54a5c28dac632a28e3262922a327bd296c
gustavo-exe <gus.elim2@gmail.com>
2021-07-31 15:20:25 -0600

    Responsive para recordatorios

--- librarian/due_handler.php
+++ librarian/due_handler.php
@@ -6,8 +6,13 @@
 
 <html>
 	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 		<title>Recordatorios para hoy</title>
 		<link rel="stylesheet" type="text/css" href="../css/global_styles.css" />
+		<link rel="stylesheet" href="css/header_librarian_style.css?v=<?PHP echo time(); ?>">
+		<link rel="stylesheet" type="text/css" href="css/home_style.css?v=<?PHP echo time();?>" />
 	</head>
 	<body>
 	
