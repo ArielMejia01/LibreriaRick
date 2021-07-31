df108f72694861e72642daec6ea680e4f8f118c4
gustavo-exe <gus.elim2@gmail.com>
2021-07-31 14:55:25 -0600

    Responsive design para registro de aministrador y resultados de busqueda del libro

--- librarian/register.php
+++ librarian/register.php
@@ -7,6 +7,8 @@
 
 <html>
 	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Registro</title>
 		<link rel="stylesheet" type="text/css" href="../css/global_styles.css">
 		<link rel="stylesheet" type="text/css" href="../css/form_styles.css">
